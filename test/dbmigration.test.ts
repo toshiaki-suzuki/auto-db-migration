5624132cb12311b7e0bfeb2e089559b6685f417d
toshiaki-suzuki <saki0x4bf@gmail.com>
2024-12-03 10:43:13 +0900

    cdk init

---
+++ test/dbmigration.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Dbmigration from '../lib/dbmigration-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/dbmigration-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Dbmigration.DbmigrationStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
