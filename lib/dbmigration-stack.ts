4095ca984bab9107b01d837a426c8a6e31872a5a
toshiaki-suzuki <saki0x4bf@gmail.com>
2024-12-03 10:55:56 +0900

    add db migration lambda function

--- lib/dbmigration-stack.ts
+++ lib/dbmigration-stack.ts
@@ -1,16 +1,15 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class DbmigrationStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'DbmigrationQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const handler = new lambda.Function(this, 'dbMigrationFunction', {
+      runtime: lambda.Runtime.PYTHON_3_11,
+      code: lambda.Code.fromAsset('functions/db_migration'),
+      handler: 'index.lambda_handler',
+    });
   }
 }
