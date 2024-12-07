56bb3a08d81e4642e8a1d31791d12e99e1b88732
toshiaki-suzuki <saki0x4bf@gmail.com>
2024-12-07 11:24:48 +0900

    move functions dir to inside of lib dir

--- lib/dbmigration-stack.ts
+++ lib/dbmigration-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as lambda_python from '@aws-cdk/aws-lambda-python-alpha';
+import * as path from 'path';
 
 export class DbmigrationStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -9,7 +10,7 @@ export class DbmigrationStack extends cdk.Stack {
 
     const handler = new lambda_python.PythonFunction(this, 'dbMigrationFunction', {
       runtime: lambda.Runtime.PYTHON_3_11,
-      entry: 'functions/db_migration',
+      entry: path.join(__dirname, 'functions', 'db_migration'),
       handler: 'index.lambda_handler',
     });
   }
