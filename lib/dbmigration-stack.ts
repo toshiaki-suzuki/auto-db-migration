073361619b0a29d9bcea10b56b137921d072b6b6
toshiaki-suzuki <saki0x4bf@gmail.com>
2024-12-07 14:36:13 +0900

    add lambda layer

--- lib/dbmigration-stack.ts
+++ lib/dbmigration-stack.ts
@@ -8,11 +8,18 @@ export class DbmigrationStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const layer = new lambda_python.PythonLayerVersion(this, "dbMigrationLayer", {
+      layerVersionName: "db-migration-layer",
+      entry: path.join(__dirname, 'lambda-layer'),
+      compatibleRuntimes: [lambda.Runtime.PYTHON_3_11],
+    });
+
     const handler = new lambda_python.PythonFunction(this, 'dbMigrationFunction', {
       functionName: 'db-migration-function',
       runtime: lambda.Runtime.PYTHON_3_11,
       entry: path.join(__dirname, 'lambda-functions', 'db_migration'),
       handler: 'index.lambda_handler',
+      layers: [layer],
     });
   }
 }
