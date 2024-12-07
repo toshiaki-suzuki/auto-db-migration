afa64b7cea110e4e00dc8567a8dc64af723afe5a
toshiaki-suzuki <saki0x4bf@gmail.com>
2024-12-07 17:10:04 +0900

    set database url in env.py

--- migrations/env.py
+++ migrations/env.py
@@ -1,13 +1,22 @@
-from logging.config import fileConfig
+import sys
+import os
 
+from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
 from alembic import context
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+from models import Base  # モデルが定義されている場所に応じて調整
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+def get_database_url():
+    from dotenv import load_dotenv
+    load_dotenv()
+    return os.getenv('DATABASE_URL')
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -18,7 +27,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -38,7 +47,9 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = get_database_url()
+    if not url:
+        raise ValueError("DATABASE_URL environment variable is not set")
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -57,8 +68,12 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
+    url = get_database_url()
+    if not url:
+        raise ValueError("DATABASE_URL environment variable is not set")
+
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
+        {'sqlalchemy.url': url},
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
