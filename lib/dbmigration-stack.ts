2245b7eed3075d6debe4dc7e91da144c019897cf
toshiaki-suzuki <saki0x4bf@gmail.com>
2024-12-08 12:07:57 +0900

    add custom resource to trigger db migration handler

--- lib/dbmigration-stack.ts
+++ lib/dbmigration-stack.ts
@@ -3,6 +3,7 @@ import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as lambda_python from '@aws-cdk/aws-lambda-python-alpha';
 import * as path from 'path';
+import { Provider } from 'aws-cdk-lib/custom-resources';
 
 export class DbmigrationStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -20,6 +21,15 @@ export class DbmigrationStack extends cdk.Stack {
       entry: path.join(__dirname, 'lambda-functions', 'db_migration'),
       handler: 'lambda_handler',
       layers: [layer],
+    });
+
+    const provider = new Provider(this, "Provider", {
+      onEventHandler: handler,
+    });
+
+    new cdk.CustomResource(this, "Custom::Migration", {
+      serviceToken: provider.serviceToken,
+      properties: {DummyValue: Date.now().toString(),}, // ダミーのプロパティを設定してリソースの更新をトリガー
     });
   }
 }
