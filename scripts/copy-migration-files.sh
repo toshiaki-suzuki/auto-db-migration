dcf72eb124b080966d559f0894ed6c6f651b7b96
toshiaki-suzuki <saki0x4bf@gmail.com>
2024-12-14 18:49:33 +0900

    fix copy migration script

--- scripts/copy-migration-files.sh
+++ scripts/copy-migration-files.sh
@@ -2,6 +2,11 @@
 
 DESTINATION_DIR="./lib/lambda-functions/db_migration"
 
+rm "$DESTINATION_DIR/alembic.ini"
+rm "$DESTINATION_DIR/database.py"
+rm "$DESTINATION_DIR/models.py"
+rm -rf "$DESTINATION_DIR/migrations"
+
 cp "./alembic.ini" "$DESTINATION_DIR/alembic.ini"
 cp "./src/database.py" "$DESTINATION_DIR/database.py"
 cp "./src/models.py" "$DESTINATION_DIR/models.py"
