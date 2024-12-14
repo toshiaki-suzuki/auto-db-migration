9ee7d8b2714ff0f3a6d9f8b4e17aae69db72eb9f
toshiaki-suzuki <saki0x4bf@gmail.com>
2024-12-14 18:48:51 +0900

    organize library

--- lib/dbmigration-stack.ts
+++ lib/dbmigration-stack.ts
@@ -4,8 +4,9 @@ import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as lambda_python from '@aws-cdk/aws-lambda-python-alpha';
 import * as path from 'path';
 import { Provider } from 'aws-cdk-lib/custom-resources';
-import { AuroraPostgresEngineVersion, ClusterInstance, DatabaseCluster, DatabaseClusterEngine, ServerlessCluster } from 'aws-cdk-lib/aws-rds';
+import { AuroraPostgresEngineVersion, ClusterInstance, DatabaseCluster, DatabaseClusterEngine } from 'aws-cdk-lib/aws-rds';
 import { InterfaceVpcEndpointAwsService, SubnetType, Vpc } from 'aws-cdk-lib/aws-ec2';
+import { CustomResource } from 'aws-cdk-lib';
 
 export class DbmigrationStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -69,7 +70,7 @@ export class DbmigrationStack extends cdk.Stack {
       onEventHandler: handler,
     });
 
-    new cdk.CustomResource(this, "Custom::Migration", {
+    new CustomResource(this, "Custom::Migration", {
       serviceToken: provider.serviceToken,
       properties: { DummyValue: Date.now().toString() }, // ダミーのプロパティを設定してリソースの更新をトリガー
     });
